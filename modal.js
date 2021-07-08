8a3ea1da597f4335ff9418f1e768de8264973d15
AlexisBoucher <alexis.boucher75017@gmail.com>
2021-07-09 00:41:08 +0200

    Creation des variables en JS / Fonctionalité pour le bouton X / Bouton submit et page  remerciement crée et bouton close fonstionelle//

--- modal.js
+++ modal.js
@@ -8,14 +8,36 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".bground");
+const modalBgSubmit = document.querySelector(".bground-submit");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalsBtn = document.querySelectorAll(".closed");
+const modalSubmit = document.querySelectorAll(".btn-submit")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+}
+
+// launch modal submit 
+
+modalSubmit.forEach((btnSubmit) => btnSubmit.addEventListener("click",modalSub));
+
+function modalSub(e){
+  e.preventDefault();
+  modalBgSubmit.style.display = "block"
+  modalBg.style.display = "none"
+}
+
+//close modal 
+
+closeModalsBtn.forEach((btnClose) => btnClose.addEventListener("click",closeModal))
+
+function closeModal (){
+  modalBg.style.display = "none"
+  modalBgSubmit.style.display = "none"
 }
